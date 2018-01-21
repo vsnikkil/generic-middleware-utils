b4ac265326824ffe8df3ff811f7241485ec12b93
Vesa Nikkilä <vsnikkil@gmail.com>
2018-01-22 00:34:45 +0200

    Default handler stubs

---
+++ src/lib/defaultHandlers.js
@@ -0,0 +1,18 @@
+// Default handlers utilize sequelize
+
+export function defaultListingHandler () {
+
+}
+
+export function defaultCreateHandler () {
+
+}
+
+export function defaultUpdateHandler () {
+
+}
+
+export function defaultUpdateHandler () {
+
+}
+
---
+++ src/lib/generateCreateMiddleware.js
@@ -0,0 +1,16 @@
+import { defaultCreateHandler } from './defaultHandlers'
+
+export function generateCreateMiddleware (handler, options) {
+  if (typeof handler !== 'function') {
+    throw new Error('`handler` must be a function')
+  }
+
+  // TODO: add handler options, i.e., validation, filtering, limits, error handling
+  return async (ctx, next) => {
+    const results = await handler()
+
+    ctx.body = results
+  }
+}
+
+
